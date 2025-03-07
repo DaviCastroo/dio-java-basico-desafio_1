8e914a622066be1f2358bf8d498cf3288e815c70
Davi Castro <davic.b.c.r@gmail.com>
2025-03-07 10:05:08 -0300

    desafio pronto!

--- conta_banco/src/ContaTerminal.java
+++ conta_banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+    final int Conta=1021;
+    final double Saldo=249.7;
+    final String Agencia="1024-2";
+    final String NomeCliente="Davi Castro";
+
+    System.out.print("Olá, seja bem vindo!, digite a seguir o número da sua agência:");
+        String leituraAgencia = scanner.nextLine();
+    
+    if (leituraAgencia.equals(Agencia)) {
+        System.out.printf("%nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", NomeCliente, Agencia, Conta, Saldo);
+    }
+    else{
+        System.out.println("Desculpe, essa agência não existe, tente novamente");
+            System.exit(0);
+    }
+
     }
 }
